5d55c2ab97531987cbb90f997c791c77c4969f1d
JongChanPark <dmdehf123@gmail.com>
2026-01-12 16:32:33 +0900

    구조적 개선을 위한 Obserber Pattern 적용

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -28,7 +28,34 @@ public class GameManager : MonoBehaviour
     public void RegisterObject(GameObject obj)
     {
         currentActiveObject = obj;
-        Debug.Log("🔒 오브젝트 등록됨: 다른 소환 차단!");
+
+        // ★ 핵심 연결 고리! ★
+        RubbableObject rubObj = obj.GetComponent<RubbableObject>();
+        if (rubObj != null)
+        {
+            // "수달아, 네가 청소 끝났다고 방송(Event)하면, 
+            // 내가 인벤토리 매니저한테 연락해서 아이템 넣으라고 시킬게."
+            rubObj.OnCleaningCompleted += HandleItemCollected;
+
+            // 나중에 수달이 사라지면 나한테도 알려줘 (내 자리 비우게)
+            rubObj.OnCleaningCompleted += HandleObjectDestroyed;
+        }
+    }
+
+    // 인벤토리에 추가하라고 시키는 함수
+    void HandleItemCollected(string itemName)
+    {
+        Debug.Log("얻은 아이템 이름! " + itemName);
+        if (InventoryManager.Instance != null)
+        { 
+            //InventoryManager.Instance.AddItem(itemName); 추후 인벤토리매니저에서 아이템 추가 함수 생기면 수정
+        }
+    }
+
+    // 자리 비우는 함수
+    void HandleObjectDestroyed(string itemName) // 매개변수 맞춰줌
+    {
+        UnregisterObject();
     }
 
     // 수집 완료해서 사라질 때 해제하기
