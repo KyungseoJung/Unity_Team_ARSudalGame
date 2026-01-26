a23a5986222d6ef618dad2aebd6d641679335e9d
JongChanPark <dmdehf123@gmail.com>
2026-01-26 16:35:52 +0900

    폰트 추가 및 오브젝트 생성 및 파괴시 텍스트 추가

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -7,42 +7,37 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance;
 
     [Header("Fixed Status UI (2D Fixed)")]
-    public GameObject nameTagPanel;    // 이름표 패널 (상단이나 하단에 고정된 부모 오브젝트)
-    public TextMeshProUGUI nameText;   // 수달 이름 텍스트
+    public TextMeshProUGUI nameText;
+    public GameObject nameTagPanel;
 
     [Header("Acquisition Popup")]
     public GameObject popupPanel;
     public TextMeshProUGUI popupText;
 
+
     void Awake()
     {
         if (Instance == null) Instance = this;
-
-        // 초기화: 모든 UI 끄기
         nameTagPanel.SetActive(false);
         popupPanel.SetActive(false);
     }
 
-    // 1. 수달이 발견되었을 때 호출 (상단 고정 UI 켜기)
     public void ShowOtterInfo(string name)
     {
-        nameText.text = name;
+        nameText.text = name + "이 나타났습니다\n깨끗하게 씻겨서 수집해주세요!";
         nameTagPanel.SetActive(true);
     }
 
-    // 2. 획득 팝업 함수
     public void ShowAcquirePopup(string itemName)
     {
-        // 이름 정보 UI는 끄고 획득 팝업 띄우기
         nameTagPanel.SetActive(false);
-
         StopAllCoroutines();
         StartCoroutine(PopupRoutine(itemName));
     }
 
     IEnumerator PopupRoutine(string itemName)
     {
-        popupText.text = $"✨ {itemName} 획득!";
+        popupText.text = $"{itemName} 획득!";
         popupPanel.SetActive(true);
         yield return new WaitForSeconds(2.0f);
         popupPanel.SetActive(false);
